0924ce586a44693345bab73622d32137ff822398
Juan Lizarazo <gabriellg4@gmail.com>
2016-06-07 21:56:47 -0600

    Update app.js ES6 for annonymous methods

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use((err, req, res, next) => {
     res.status(err.status || 500);
     delete err.status;
     console.error(err);
@@ -47,7 +47,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   res.status(err.status || 500);
   res.json({
     message: err.message,
